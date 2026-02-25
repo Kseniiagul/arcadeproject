65860484bfedf9a65008fabb2c7b76f376cd1ab4
arina-minina <aroma.minina.serg@gmail.com>
2026-02-25 15:55:47 +0300

    new constants
    constants for player.py

--- melting_point/src/constants.py
+++ melting_point/src/constants.py
@@ -1,3 +1,29 @@
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Melting Point"
+
+PLAYER_SCALE = 0.3
+
+AVAILABLE_SKINS = [
+    {
+        "name": "Классика",
+        "file": "player.png",
+        "game_scale": 0.3,
+        "menu_scale": 0.5
+    },
+    {
+        "name": "Слизень",
+        "file": "player_slime.png",
+        "game_scale": 0.2,
+        "menu_scale": 0.3
+    },
+    {
+        "name": "Кристалл",
+        "file": "player_pink.png",
+        "game_scale": 0.2,
+        "menu_scale": 0.3
+    }
+]
+
+PLAYER_START_X = 100
+PLAYER_START_Y = 300
